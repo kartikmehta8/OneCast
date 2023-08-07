2e59f5a2277948214008276d524014827f2634a8
Firas <firasmumtaz12@gmail.com>
2023-08-08 02:47:25 +0530

    ReadMe Created

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
   const [data, setData] = useState();
 
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios.get('http://localhost:5000/');
+      const result = await axios.get("http://localhost:3000/");
       setData(result.data);
     };
     fetchData();
   }, []);
 
-  return <div className='App'>{JSON.stringify(data)}</div>;
+  return <div className="App">{JSON.stringify(data)}</div>;
 }
 
 export default App;
---
+++ readme.md
@@ -0,0 +1,3 @@
+## OneCast
+
+OneCast is our narrow cast web app used to send messages to various social messaging apps including Telegram, Discord and Slack.
