e202036ad799223b97f1d4f305511b3f8ad6995d
Kartik Mehta <kartikmehta202@gmail.com>
2023-08-07 23:46:39 +0530

    onecast

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get('http://localhost:5000/');
+      setData(result.data);
+    };
+    fetchData();
+  }, []);
+
+  return <div className='App'>{JSON.stringify(data)}</div>;
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+const compression = require('./middlewares/Compression');
+const clusterMiddleware = require('./middlewares/Clusters');
+const dotenv = require('dotenv');
+const bodyParser = require('./middlewares/BodyParser');
+const morgan = require('./middlewares/Morgan');
+
+// Routes Import
+const sampleRoutes = require('./routes/sample');
+
+dotenv.config();
+
+const app = express();
+
+// Middlewares
+app.use(compression);
+clusterMiddleware(app);
+app.use(cors());
+app.use(bodyParser);
+app.use(morgan);
+
+// Routes
+app.use('/sample', sampleRoutes);
+
+app.get('/', (req, res) => {
+  res.send({
+    server: 'Express',
+    status: 'OK',
+  });
+});
