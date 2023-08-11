735c4d1b78c08d47edd984c882261c56a8489129
Kartik Goel <kartik.2024it1170@kiet.edu>
2023-08-11 18:28:55 +0530

    update 	modified:   server/controllers/discordController.js

--- server/controllers/discordController.js
+++ server/controllers/discordController.js
@@ -6,8 +6,7 @@ async function sendMessage(req, res, next) {
 
     // Create a message to be sent to the Discord channel
     const message = {
-      content,
-      username: "Tushar"
+      content
     };
 
     const response = await axios({
