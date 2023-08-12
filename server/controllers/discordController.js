11759e6d223d61aa8b292c4a931922f3cc0f20db
Kartik Goel <kartik.2024it1170@kiet.edu>
2023-08-12 19:01:56 +0530

    modified:   server/controllers/discordController.js

--- server/controllers/discordController.js
+++ server/controllers/discordController.js
@@ -1,28 +1,20 @@
 const axios = require('axios');
 
-async function sendMessage(req, res, next) {
+async function sendMessage(req, res) {
   try {
-    const { body, webhookUrl } = req.body;
-
-    // Create a message to be sent to the Discord channel
-    const message = {
-      body
-    };
-
+    const { body } = req.body;
+    const message = { body };
     const response = await axios({
       method: "POST",
       url: webhookUrl = PROCESS.env.DISCORD_WEBHOOK_URL,
       data: message
     })
-    console.log(response.status);
 
-    res.status(200).json({ message: 'Message sent successfully' });
+    res.status(200).json({ success: true });
   } catch (error) {
-    console.error('Error sending message:', error);
-    res.status(500).json({ error: 'An error occurred while sending the message' });
+    res.status(500).json({ success: false, error });
   }
 }
-
 module.exports = {
   sendMessage,
 };
