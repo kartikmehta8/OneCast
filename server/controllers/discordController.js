4e3f2b5edd3d2564b45e721d00331abd3c378363
Kartik Goel <kartik.2024it1170@kiet.edu>
2023-08-13 02:43:54 +0530

    modified:   server/controllers/discordController.js 	modified:   server/package.json

--- server/controllers/discordController.js
+++ server/controllers/discordController.js
@@ -1,19 +1,35 @@
 const axios = require('axios');
+const webhookURL = 'https://discord.com/api/webhooks/1139542261881905253/Ja9FhJQmnweVq27h9euq0R-z9X1gWC4-wpD8hFep7Fc1DLWlRc6CbzdkFc1opEh2Hobf';
 
-async function sendMessage(req, res, next) {
+async function sendMessageToChannel(message) {
   try {
-    const { body } = req.body;
-    const webhookURL = process.env.WEBHOOK_URL;
-    
     const response = await axios.post(webhookURL, {
-      content: body
+      content: message
     });
-
-    res.status(200).json({ success: true });
+    return { status: 'OK', data: response.data };
   } catch (error) {
-    res.status(500).json({ success: false, error: error.message });
+    return { status: 'ERROR', data: error.response ? error.response.data : error.message };
   }
 }
+
+async function sendMessage(req, res, next) {
+    const { body } = req.body;
+    const { status, data } = await sendMessageToChannel(body);
+
+    if (status === 'OK') {
+      res.send({
+        status: 'OK',
+        success: true,
+        data: data,
+      });
+    } else {
+      res.send({
+        status: 'ERROR',
+        success: false,
+        data: data,
+      });
+    }
+};
 
 module.exports = {
   sendMessage,
