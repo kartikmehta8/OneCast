4f66955e8c74da5ed4af6b3b1a84f17fa36294c8
Kartik Goel <kartik.2024it1170@kiet.edu>
2023-08-13 01:14:57 +0530

    updates 	modified:   server/controllers/discordController.js 	modified:   server/package.json 	modified:   server/routes/discord.js

--- server/controllers/discordController.js
+++ server/controllers/discordController.js
@@ -1,20 +1,20 @@
 const axios = require('axios');
 
-async function sendMessage(req, res) {
+async function sendMessage(req, res, next) {
   try {
     const { body } = req.body;
-    const message = { body };
-    const response = await axios({
-      method: "POST",
-      url: webhookUrl = PROCESS.env.DISCORD_WEBHOOK_URL,
-      data: message
-    })
+    const webhookURL = 'https://discord.com/api/webhooks/1139542261881905253/Ja9FhJQmnweVq27h9euq0R-z9X1gWC4-wpD8hFep7Fc1DLWlRc6CbzdkFc1opEh2Hobf';
+    
+    const response = await axios.post(webhookURL, {
+      content: body
+    });
 
     res.status(200).json({ success: true });
   } catch (error) {
-    res.status(500).json({ success: false, error });
+    res.status(500).json({ success: false, error: error.message });
   }
 }
+
 module.exports = {
   sendMessage,
 };
